0fd0fa02eb09cab91777cedcba4ba43d55a36f10
Fred Vernier <frederic.vernier@gmail.com>
2024-12-13 16:45:39 +0100

    1rst attempt to make a library

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
     })
   ],
   output: {
+    library: {
+      name: "DensityMaps",
+      type: "umd"
+    },
     //path: path.resolve(__dirname, 'public/js'),
     filename: 'js/client.js'
   }
